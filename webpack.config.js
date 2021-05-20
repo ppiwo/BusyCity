31fc3f9fed450f2597a44cfcc418a5d2f12a3f1b
Patrick Piwowarczyk <piwowarczykpatrick@gmail.com>
2021-05-20 10:36:41 -0500

    oops - forgot add webpack config to commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,51 @@
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require("path");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    watch: true,
-    module: {
-      rules: [
-        {
-          test: /\.(scss|css)$/,
-          use: ["style-loader", "css-loader", "sass-loader"]
-        },
-        {
-          test: /\.(png|jpe?g|gif|kml|kmz)$/i,
-          use: [
-            {
-              loader: "file-loader"
-            }
-          ]
-        },
-        {
-          test: /\.hbs$/,
-          loader: "handlebars-loader",
-          options: {
-              helperDirs: path.resolve(__dirname, "src", "./templates/helpers")
+  mode: 'production',
+  watch: false,
+  module: {
+    rules: [
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader?url=false', 'resolve-url-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|kml|kmz|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader'
           }
-        },
-      ],
-    },
-    plugins: [
-      new webpack.DefinePlugin({ 'process.env':{ 'API_KEY': 'test999'} }),
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src", "index.html")
-      })
+        ]
+      },
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader',
+        options: {
+          helperDirs: path.resolve(__dirname, 'src', './templates/helpers')
+        }
+      }
     ]
-  };
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed)
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './src/assets', to: './assets' }]
+    })
+  ],
+  output: {
+    clean: true
+  }
+};
